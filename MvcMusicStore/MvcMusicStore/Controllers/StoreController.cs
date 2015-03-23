8c2d69110a38843ff30c9365c71fd38add0d3e12
Nes-Gonzalez <N_gonzalez@live.com>
2015-03-22 19:01:47 -0500

    Completed through section 3 of docs
    I completed all sets up to section 4, program is now ready to implement
    database

--- MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.ViewModels;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
@@ -11,23 +13,49 @@ namespace MvcMusicStore.Controllers
         //
         // GET: /Store/
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello from Store.Index()";
+            //create a list of genres
+            var genres = new List<string> { "Rock", "Jazz", "Country", "Pop", "Disco" };
+
+            //create our view model
+            var viewModel = new StoreIndexViewModel
+            {
+                NumberOfGenres = genres.Count(),
+                Genres = genres
+            };
+
+            return View(viewModel);
         }
 
         //GET: /Store/Browse?genre=?Disco
-        public string Browse(string genre)
+        public ActionResult Browse(string genre)
         {
-            string message = HttpUtility.HtmlEncode("Store.Browse, Genre= " + genre);
-            return message;
+            var genreModel = new Genre
+            {
+                Name = genre
+            };
+
+            var albums = new List<Album>()
+            {
+                new Album { Title = genre + " Album 1"},
+                new Album { Title = genre + " Album 2"}
+            };
+
+            var viewModel = new StoreBrowserViewModel
+            {
+                Genre = genreModel,
+                Albums = albums
+            };
+            return View(viewModel);
         }
 
         //GET: /Store/Details/5
-        public string Details(int id)
+        public ActionResult Details(int id)
         {
-            string message = "Store.Details, ID = " + id;
-            return message;
+            var album = new Album { Title = "Sample Album" };
+
+            return View(album);
         }
     }
 }
